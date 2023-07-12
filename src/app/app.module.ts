1c0df25c480d1e4522850bd7d0e56b2193bbb1e0
profmoriartey <kaito9814@gmail.com>
2023-07-12 13:46:12 +0300

    Adding lesson-material component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LessonMaterialComponent } from './lesson-material/lesson-material.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LessonMaterialComponent
   ],
   imports: [
     BrowserModule,
